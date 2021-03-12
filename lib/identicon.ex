421c12e45081539e589261091a9f5c44c79e2a06
Darren Jensen <darren.jensen@gmail.com>
2021-03-12 08:48:20 +0700

    added mirror_row function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,17 +1,4 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
-  """
   def main(input) do
     input
     |> hash_input
@@ -19,9 +6,15 @@ defmodule Identicon do
     |> build_grid
   end
 
-  def build_grid(%Identicon.Image{hex: hex} = image) do
+  def build_grid(%Identicon.Image{hex: [_head | hex ]} = image) do
     hex
     |> Enum.chunk_every(3)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
